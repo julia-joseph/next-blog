78eb96dbd5a94ef097940e79c4028276df991423
Julia Joseph <juljoe726@gmail.com>
2021-08-07 21:34:36 +0530

    adding profile image to home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
-const HomePage = props => {
-    return <h1>Home Page</h1>
-}
+import classes from "../styles/home.module.css";
+
+const HomePage = (props) => {
+  return (
+    <div className={classes.profileContainer}>
+      <img
+        src="images/607866.png"
+        alt="profile picture"
+        className={classes.profilePicture}
+      ></img>
+    </div>
+  );
+};
 
 export default HomePage;
