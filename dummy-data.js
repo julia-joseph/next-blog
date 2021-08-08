42b973d90deb263d0bcc427d93a21de2f1b18fa0
Julia Joseph <juljoe726@gmail.com>
2021-08-08 22:41:29 +0530

    refactoring dummy posts to const

---
+++ dummy-data.js
@@ -0,0 +1,34 @@
+export const DUMMY_POSTS = [
+  {
+    id: 0,
+    title: "Play time",
+    date: "10th May 2021",
+    description: "Play it out with the pals",
+    image: "images/607866.png",
+    featured: false,
+  },
+  {
+    id: 1,
+    title: "Watch and learn",
+    date: "21st Aug 2021",
+    description: "Play it out with the pals",
+    image: "images/607866.png",
+    featured: false,
+  },
+  {
+    id: 2,
+    title: "Round and roses",
+    date: "5th Sept 2021",
+    description: "Play it out with the pals",
+    image: "images/607866.png",
+    featured: true,
+  },
+  {
+    id: 3,
+    title: "Catch the dragonfly",
+    date: "9th Sept 2021",
+    description: "Play it out with the pals",
+    image: "images/607866.png",
+    featured: true,
+  },
+];
