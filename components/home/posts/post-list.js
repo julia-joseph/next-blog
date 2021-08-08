c883a52d5571d948c152c07f7a7c0a07df33603d
Julia Joseph <juljoe726@gmail.com>
2021-08-08 22:08:58 +0530

    creating post-list component

---
+++ components/home/posts/post-list.js
@@ -0,0 +1,15 @@
+import classes from "../../../styles/post-list.module.css";
+import Post from "./post";
+
+const PostList = (props) => {
+  const { posts } = props;
+  return (
+    <div className={classes.container}>
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
+};
+
+export default PostList;
